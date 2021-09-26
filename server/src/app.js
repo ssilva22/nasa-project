cb38b47989540bae967441cc97702c576d916695
Sebastian <sebastianwebp@gmail.com>
2021-09-26 11:17:57 -0400

    configured server to start serving react frontend and added morgan as a logging middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path")
 const express = require("express")
 const cors = require("cors")
+const morgan = require("morgan")
 
 const planetsRouter = require("./routes/planets/planets.router")
 
@@ -10,7 +12,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 )
+
+app.use(morgan("combined"))
+
 app.use(express.json())
 app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
 
 module.exports = app
