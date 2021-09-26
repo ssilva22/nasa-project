a6ce1b3d5c878058ce5cfcfe8f38d571e3515c92
Sebastian <sebastianwebp@gmail.com>
2021-09-26 12:05:17 -0400

    added launches model and controller

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 
 const planetsRouter = require("./routes/planets/planets.router")
+const launchesRouter = require("./routes/launches/launches.router")
 
 const app = express()
 
@@ -17,9 +18,10 @@ app.use(morgan("combined"))
 
 app.use(express.json())
 app.use(planetsRouter)
+app.use(launchesRouter)
 app.use(express.static(path.join(__dirname, "..", "public")))
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
 
